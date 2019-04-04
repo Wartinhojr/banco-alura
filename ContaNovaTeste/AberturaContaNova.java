26df5b9cb0eb07b72ae17a827c849c992558cc98
Willians Lopes <willians.p.lopes@gmail.com>
2019-04-04 12:06:16 -0400

    Coloquei as duas formas que tentei fazer e não consegui

---
+++ ContaNovaTeste/AberturaContaNova.java
@@ -0,0 +1,9 @@
+
+public class AberturaContaNova {
+
+	public static void main(String[] args) {
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 200;
+		System.out.println(primeiraConta.saldo);
+	}
+}
